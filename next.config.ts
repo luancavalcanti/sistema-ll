33f2ceb70fdb7072c23b781bf7a946c936989f4c
luancavalcanti <luanscavalcanti@gmail.com>
2026-03-30 15:44:41 -0400

    feat: Add redirects configuration to handle root path navigation

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,17 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ['10.0.0.89'],
   reactCompiler: true,
   basePath: '/sistema2',
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/sistema2',
+        basePath: false, // 👈 Importante: impede que ele procure /sistema2/sistema2
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
