42a223c66ebfd529816fc2d823a48f09303a859f
luancavalcanti <luanscavalcanti@gmail.com>
2026-04-06 22:47:40 -0400

    Removendo config invalida e criando eslintignore

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,6 @@ const nextConfig = {
   allowedDevOrigins: ['10.0.0.89'],
   reactCompiler: true,
 
-  // Ignora erros do ESLint na hora do build na Vercel
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  
   // Ignora erros de tipagem (any) na hora do build na Vercel
   typescript: {
     ignoreBuildErrors: true,
